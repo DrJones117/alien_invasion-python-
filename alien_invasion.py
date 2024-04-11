3c5dd92277b649f9a8659fda68399a05483c8b1b
Judah Kahler <judah.kahler@gmail.com>
2024-04-11 16:36:42 -0500

    Add the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     # Parent class for game assets and behavior
@@ -16,22 +17,31 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set the background color
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # Start the main loop for the game.
         while True:
-            # The loop watches for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
+            self._check_events()
+            self._update_screen()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    # Two helper functions
+    def _check_events(self):
+        # The loop watches for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 
 if __name__ == '__main__':
---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship:
+    # The Ship class
+    def __init__(self, ai_game):
+        # Intialize the ship and set its starting position.
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/rocket-2442125_640.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start the ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        # Draw the ship at its surrent location.
+        self.screen.blit(self.image, self.rect)
